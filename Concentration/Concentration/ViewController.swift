670d85effcd46133fec1320ec98ce2902c586acc
chbui <sibulabu@gmail.com>
2018-09-04 22:29:06 -0500

    started on Cencentration game lesson

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Chuong Bui on 9/4/18.
+//  Copyright © 2018 Chuong Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: 👻, on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        
+    }
+
+}
+
